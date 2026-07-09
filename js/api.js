18bf394aa28f8fa3f5d5b9ca8455327530da8345
H4R0LDAM <pool.mestanza@gmail.com>
2026-07-09 02:04:49 -0500

    Proyecto HTTPS

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
-const INVENTARIO_URL = "http://3.143.219.58:30080/productos";
-const REPORTE_URL = "http://3.143.219.58:30081/ventas";
+const INVENTARIO_URL = "https://3-143-219-58.sslip.io/productos";
+const REPORTE_URL = "https://3-143-219-58.sslip.io/ventas";
 
 async function get(url) {
 
